627f949cb8d7c31cd6b784cedc37ed39367d2cf6
GleamTech <marketing@gleamtech.com>
2018-06-25 07:36:03 -0700

    Version 3.1.5
    ## Version 3.1.5 - June 25, 2018
    
      - **Improved:** Stability of image processing.
    
      - **Improved:** Documentation.

--- Examples/AspNetCore.CS/Startup.cs
+++ Examples/AspNetCore.CS/Startup.cs
@@ -24,10 +24,6 @@ namespace GleamTech.ImageUltimateExamples.AspNetCore.CS
             services.AddMvc();
 
             services.AddGleamTech();
-
-            var licenseFile = Hosting.ResolvePhysicalPath("~/App_Data/License.dat");
-            if (File.Exists(licenseFile))
-                ImageUltimateConfiguration.Current.LicenseKey = File.ReadAllText(licenseFile);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +40,10 @@ namespace GleamTech.ImageUltimateExamples.AspNetCore.CS
             }
 
             app.UseGleamTech();
+
+            var licenseFile = Hosting.ResolvePhysicalPath("~/App_Data/License.dat");
+            if (File.Exists(licenseFile))
+                ImageUltimateConfiguration.Current.LicenseKey = File.ReadAllText(licenseFile);
 
             app.UseStaticFiles();
 
